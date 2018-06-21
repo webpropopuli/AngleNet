58a9d8dbe439c85a663f4c9b05f9d8b37168e1bf
David Marlowe <webpropopuli@gmail.com>
2018-06-21 13:50:50 -0400

    Mode environment fun....

--- AngleNet2/ClientApp/app/components/home/home.component.ts
+++ AngleNet2/ClientApp/app/components/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html '
 })
 export class HomeComponent {
 }
